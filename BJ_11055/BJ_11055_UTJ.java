4adbe4a5fd2b369b50aea74eb5d917cd200927a7
ttajun36 <31652115+ttajun36@users.noreply.github.com>
2020-05-21 23:00:43 +0900

    GAS

--- BJ_11055/BJ_11055_UTJ.java
+++ BJ_11055/BJ_11055_UTJ.java
@@ -1,39 +1,35 @@
 import java.util.*;
 
-class Q11055 
+class Q11055Second
 {
-	public static void funcQ(int[] arrA, int[] arrQ, int i){
-		int newAns = arrA[i];
-		int nowInt = arrA[i];
-
-		for (int j=1;j<=i ;j++ )
-			if (nowInt > arrA[i-j]){
-				newAns += arrA[i-j];
-				nowInt = arrA[i-j];
-			}
-
-		if (newAns > arrQ[i-1])
-			arrQ[i] = newAns;
-		else
-			arrQ[i] = arrQ[i-1];
-	}
-
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
 
 		int arrSize = sc.nextInt();
-		int[] arrA = new int[arrSize];
+		int[] arrN = new int[arrSize];
 		for (int i=0;i<arrSize ;i++ )
-			arrA[i] = sc.nextInt();
+			arrN[i] = sc.nextInt();
 		sc.close();
 
-		int[] arrQ = new int[arrSize];
-		arrQ[0] = arrA[0];
-		
-		for (int i=1;i<arrSize ;i++ )
-			funcQ(arrA, arrQ, i);
+		int[] arrA  = new int[arrSize];
+		arrA[0]=arrN[0];
 
-		System.out.println(arrQ[arrSize-1]);
+		int maxSum;
+		for (int i=1;i<arrSize ;i++ )
+		{
+			maxSum=arrN[i];
+			for (int j=0;j<i ;j++ )
+				if (arrN[i-1-j]< arrN[i] && arrA[i-1-j]+arrN[i]>maxSum)
+						maxSum=arrA[i-1-j]+arrN[i];
+			arrA[i]=maxSum;
+		}
+
+		int ans=arrA[0];
+		for (int i=1;i<arrSize ;i++ )
+			if (arrA[i]>ans)
+				ans = arrA[i];
+
+		System.out.println(ans);
 	}
 }
