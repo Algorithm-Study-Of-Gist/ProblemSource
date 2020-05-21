1772f061dbe38b2ec08c7eebf584d33ef768e4dc
ttajun36 <31652115+ttajun36@users.noreply.github.com>
2020-05-21 23:19:56 +0900

    GAS

---
+++ BJ_10835/BJ_10835_UTJ.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Q10835
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] left = new int [n];
+		for (int i=0;i<n ;i++ )
+			left[i] = sc.nextInt();
+		int[] right = new int [n];
+		for (int i=0;i<n ;i++ )
+			right[i] = sc.nextInt();
+	}
+}
